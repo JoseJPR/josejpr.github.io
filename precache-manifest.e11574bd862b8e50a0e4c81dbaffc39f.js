8c4cec62987c12778ea2bc1cebf30d4eab55f0cc
JoseJPR <me@josejpr.com>
2020-08-14 12:49:21 +0200

    Update with React Application Builded

---
+++ precache-manifest.e11574bd862b8e50a0e4c81dbaffc39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1da523f1b7585c3b38025282e4a3998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7816556b14f5b20c2bf4",
+    "url": "/static/css/main.9811d8ed.chunk.css"
+  },
+  {
+    "revision": "67815a909524b1fa6920",
+    "url": "/static/js/2.e78b1921.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e78b1921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7816556b14f5b20c2bf4",
+    "url": "/static/js/main.19fe00d5.chunk.js"
+  },
+  {
+    "revision": "e79a7b97f0658f060759",
+    "url": "/static/js/runtime-main.4f47c6ae.js"
+  }
+]);
